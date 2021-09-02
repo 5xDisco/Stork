9832bf59be93d3e6dfecc671da519b980a92efd4
linchiquei <cclok922@gmail.com>
2021-09-02 13:09:41 +0800

    增加 新增防呆 刪除防呆

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -11,9 +11,9 @@ class ChannelsController < ApplicationController
     @channel = Channel.new(channel_params);
     if(@channel.save)
       redirect_to @channel
-    else
-      redirect_to "/channels"
     end
+    @errors = @channel.errors.full_messages
+    # render remote:false ? create.html.erb : create.js.erb 
   end
 
   def new
@@ -25,18 +25,25 @@ class ChannelsController < ApplicationController
     # render json: @leave
   end
 
+
   def destroy
     @channel = Channel.find(params[:id])
+    cant_delete_channel = Channel.find_by(status:2)
+
     if(@channel.destroy)
-      redirect_to "/channels"
+      #導向不能刪的那一個
+      redirect_to cant_delete_channel 
     else
       redirect_to @channel
     end
   end
 
+  def update
+  end
+
   private
   def channel_params
-    params.require(:channel).permit(:name, :description)
+    params.require(:channel).permit(:name, :description, :status)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_103300) do
+ActiveRecord::Schema.define(version: 2021_09_02_045007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_103300) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
+    t.integer "status"
     t.index ["deleted_at"], name: "index_channels_on_deleted_at"
   end
 

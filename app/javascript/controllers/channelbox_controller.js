448992c7bd063b6476a59012c370c56311533021
linchiquei <cclok922@gmail.com>
2021-09-03 22:09:30 +0800

    解決 turbolinks 閃現

--- app/javascript/controllers/channelbox_controller.js
+++ app/javascript/controllers/channelbox_controller.js
@@ -3,12 +3,8 @@ import { Controller } from "stimulus";
 export default class extends Controller {
     static targets = [ "page" ]
 
-    connect() {
-        console.log(`new.js.erb do stimulus js`);
-    }
-
     close(){
         this.pageTarget.remove();
-        console.log(`new.js.erb destory self`)
+        console.log(`destory self`)
     }
 }

38cf6d40c9e63c18610fd5603f6d2585adb83397
Miffy Wang <4000p049@gmail.com>
2021-09-05 16:45:46 +0800

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+
+  defaultExtractor: content => {
+    const broadMatches = content.match(/[^<>"' `\s]*[^<>"'` \s:]/g) || []
+    const innerMatches = content.match(/[^<>"' `\s.()]*[^<>"'` \s.():]/g) || []
+
+    return broadMatches.concat(innerMatches)
+  },
+})
+
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
+  plugins: [require('tailwindcss'), require('autoprefixer'), ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])],
 }

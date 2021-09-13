dfb742c9d3b459ce0ae72d7e9bdc92006a336588
linchiquei <cclok922@gmail.com>
2021-09-14 00:55:09 +0800

    更新頻道畫面

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,11 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 // require("jquery");
-import "@fortawesome/fontawesome-free/css/all";
-import '@fortawesome/fontawesome-free/scss/solid';
+// import "@fortawesome/fontawesome-free/css/all";
+// import '@fortawesome/fontawesome-free/scss/solid';
 import '@fortawesome/fontawesome-free/css/v4-shims';
 import "../stylesheets/application"
 import "../channels"
 import '../spaces'
 import "../controllers"
-import "../page-layout"
+//import "../page-layout"

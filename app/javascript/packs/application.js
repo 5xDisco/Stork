41c4f1eae4cd5b0d54288df51288aedeaaddbbdb
KtKeaton <ktr01@protonmail.com>
2021-09-15 12:44:36 +0800

    修正gitignore與application.js內容同步問題

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,6 @@ import * as ActiveStorage from "@rails/activestorage"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "@fortawesome/fontawesome-free/css/all";
-import '@fortawesome/fontawesome-free/scss/solid';
-import '@fortawesome/fontawesome-free/css/v4-shims';
 import "../stylesheets/application"
 import "../channels"
 import '../spaces'

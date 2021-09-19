878ea46b4c9771c72d79c6908fcb7cd6590b11b2
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-09-20 02:36:52 +0800

    set up unreads channel and controller

---
+++ app/javascript/controllers/unreads_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "stimulus";
+import consumer from "../channels/consumer";
+
+export default class extends Controller {
+  connect() {
+    console.log(
+      'Will create subscription to: channel: "UnreadsChannel" channel_id: ' +
+        this.data.get("id")
+    );
+
+    this.channel = consumer.subscriptions.create(
+      {
+        channel: "UnreadsChannel",
+        channel_id: this.data.get("id"),
+      },
+      {
+        connected: this._cableConnected.bind(this),
+        disconnected: this._cableDisconnected.bind(this),
+        received: this._cableReceived.bind(this),
+      }
+    );
+  }
+
+  disconnect() {
+    consumer.subscriptions.remove(this.channel);
+  }
+
+  _cableConnected() {
+    console.log("_cableConnected");
+  }
+
+  _cableDisconnected() {
+    console.log("_cableDisconnected");
+  }
+
+  _cableReceived(data) {
+    console.log("_cableReceived");
+    this.element.classList.add("font-bold");
+  }
+}

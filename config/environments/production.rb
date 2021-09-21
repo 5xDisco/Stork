ef7540062b94cb3f9ddb5e4786000052403bfe6b
Miffy Wang <4000p049@gmail.com>
2021-09-21 11:43:02 +0800

    Use google to send a letter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false  #測試可改true，但如果已上線的話建議改回false
   config.action_mailer.default_url_options = { host: ENV["WEB_PATH"]} #production的絕對網址
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,

c5830b1428ea0845bda43e9a916ce6d35aa86639
linchiquei <cclok922@gmail.com>
2021-09-21 23:01:32 +0800

    設定 production mailer delivery method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :mailgun
   config.action_mailer.smtp_settings = {
     address:              ENV['smtp_address'],
     port:                  587,

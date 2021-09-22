388ef9844242182abcb35225d49e8e02cc16e446
linchiquei <cclok922@gmail.com>
2021-09-22 14:25:39 +0800

    寫錯 email

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,7 +2,7 @@ class InviteMailer < ApplicationMailer
     def invite(user: nil, email: nil)
         if user 
         @user = user.email
-        mail to: @user.email.to_s, subject:"邀請你加入Stork的行列"
+        mail to: @user.to_s, subject:"邀請你加入Stork的行列"
         else
         @user = email
         mail to: email.to_s, subject:"邀請你加入Stork的行列"

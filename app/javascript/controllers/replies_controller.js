200721e19a5e2acecb388f13eb67efc8c5dbd5fd
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-09-23 22:00:12 +0800

    add reply container box

--- app/javascript/controllers/replies_controller.js
+++ app/javascript/controllers/replies_controller.js
@@ -12,4 +12,14 @@ export default class extends Controller {
     this.element.classList.toggle("bg-gray-300");
     this.replyBtnTarget.classList.toggle("invisible");
   }
+
+  reply() {
+    const storkPanel = document.querySelector(".stork-panel");
+    const messageReplies = document.querySelector(".message-replies");
+
+    storkPanel.classList.remove("flex-1");
+    storkPanel.classList.add("w-2/3");
+    messageReplies.classList.remove("hidden");
+    messageReplies.classList.remove("absolute");
+  }
 }

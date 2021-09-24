1b8937a1233a6d4a701de90099d180920b0b938c
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-09-24 16:46:23 +0800

    add reply messages

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,17 +3,17 @@ class RepliesController < ApplicationController
   before_action :set_message
 
   def create
-    @reply = @message.replies.create(message_params)
+    @reply = @message.replies.create(reply_params)
     # MessageChannel.broadcast_to @reply, message: render_to_string(@reply)
   end
 
   private
 
   def set_message
-    @message = current_user.messages.find(params[:message_id])
+    @message = Message.find(params[:message_id])
   end
 
   def reply_params
-    params.require(:replies).permit(:content).merge(user: current_user)
+    params.require(:reply).permit(:content).merge(user: current_user)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :channel
 
-  has_many :replies
+  has_many :replies, dependent: :destroy
 
   after_create :process_mentions
 

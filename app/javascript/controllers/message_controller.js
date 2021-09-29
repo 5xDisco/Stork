92f4f3fe4f5bd5af950c0070e9ae9fb45529f03b
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-09-29 13:09:53 +0800

    Fix: unread msg style change

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -4,14 +4,11 @@ export default class extends Controller {
   static targets = ["replyBtn", "replyRoom"];
 
   emerge() {
-    this.element.classList.toggle("bg-gray-300");
     this.replyBtnTarget.classList.toggle("invisible");
-    // this.replyRoomTargrt.classList.toggle("invisible");
   }
 
   fade() {
-    this.element.classList.toggle("bg-gray-300");
     this.replyBtnTarget.classList.toggle("invisible");
-    // this.replyRoomTargrt.classList.toggle("invisible");
+
   }
 }

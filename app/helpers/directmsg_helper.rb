76d85306cc7aadf92c09354e66c7eb4419897aa1
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-09-29 13:40:53 +0800

    fix/unread-count-styling

--- app/helpers/directmsg_helper.rb
+++ app/helpers/directmsg_helper.rb
@@ -1,12 +1,10 @@
 module DirectmsgHelper
-
   def directmsg_exist(current_user, user, space)
     channel = Channel.direct_message_for_users([current_user, user], space.id)
-    classes = ['ml-auto', 'text-white', 'w-8', 'bg-red-600', 'rounded-full', 'text-center', 'inline-block']
+    classes = ['ml-3', 'text-white', 'text-xs', 'w-4', 'h-4', 'bg-red-600', 'rounded-full', 'text-center']
     if channel
       classes << (channel.has_unread_message?(current_user, space) ? 'visible' : 'invisible')
     end  
     tag.div "#{channel.unread_message_count(user)}", class: classes, "data-controller": "unreads", "data-unreads-channelId": "#{channel.id}", "data-unreads-userId": "#{current_user.id}"
   end
-
 end
